d6f2f88897910a37f30007984cdb9ee412d5724a
bangloresekar <bangaloresekar@gmail.com>
2025-05-15 11:37:17 +0530

    Update app.js
    Excellent

--- ticket-service/src/app.js
+++ ticket-service/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-
+// comment
 const app = express();
 app.use(bodyParser.json());
 app.use('/api', routes);
